039e7fa2b09aed24cff4c48bd8e70cb1a7e7a0ef
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-20 13:13:00 +0545

    Handlebars Template for Dynamic

--- playground/web-server/src/app.js
+++ playground/web-server/src/app.js
@@ -3,9 +3,31 @@ const path = require("path")
 
 const app = express()
 const PublicDirecPath = path.join(__dirname, "../public")
+// app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs')
 
 app.use(express.static(PublicDirecPath))
 
+app.get("/",(req,res) => {
+    res.render("index",{
+        title: "Home",
+        name: "Ashish Pokhrel"
+    })
+})
+
+app.get("/about", (req,res) => {
+    res.render ("about",{
+        title: "About",
+        name: "Ashish Pokhrel"
+    })
+})
+
+app.get("/help", (req,res) => {
+    res.render ("help",{
+        Message: "How are you. I hope you are fine and doing very well!!!",
+        Title: "Help"
+    })
+})
 
 app.get("/weather", (req,res)=> {
     res.send({
