08f0899274d8a658cfe6fcb302dc4eff535e3eed
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-20 13:42:28 +0545

    Customizing Views Directory

--- playground/web-server/src/app.js
+++ playground/web-server/src/app.js
@@ -2,10 +2,16 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-const PublicDirecPath = path.join(__dirname, "../public")
-// app.set('views', __dirname + '/views');
-app.set('view engine', 'hbs')
 
+//Define Paths for Express COnfig
+const PublicDirecPath = path.join(__dirname, "../public")
+const viewPath = path.join (__dirname, "../templates")
+
+//Setup handlebars engine and views location 
+app.set('view engine', 'hbs')
+app.set("views", viewPath)
+
+//Setup static directory to serve
 app.use(express.static(PublicDirecPath))
 
 app.get("/",(req,res) => {
