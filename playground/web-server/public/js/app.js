6a6f80b6ce4f056ca41fce11e796cd431e835f87
meashishpokhrel <meashishpokhrel@gmail.com>
2020-06-20 20:49:14 +0545

    Fetch

--- playground/web-server/public/js/app.js
+++ playground/web-server/public/js/app.js
@@ -1,1 +1,20 @@
 console.log("This is javscript from public.")
+
+fetch("http://puzzle.mead.io/puzzle").then((res) => {
+    res.json().then ((data) =>{
+        console.log(data);  
+    })  
+})
+
+fetch("http://localhost:3000/weather?address=kathmandu").then((res) => {
+    res.json().then((data) => {
+        if(data.err){
+            console.log(data.err)
+        }
+        else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+        
+    })
+})
